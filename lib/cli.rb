1d78128019d3ca8460f510c827bda5d515aa033d
Alexis Dorn <alexisadorn@gmail.com>
2017-05-15 02:21:03 +0000

    Adding colors

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,14 +2,13 @@ class CLI
   PETS = ["Dogs", "Cats", "Rabbits", "Birds", "Equine", "Pigs", "Barnyard", "Small"]
 
   def call
-    puts "\nWelcome to Best Friend Finder!"
-    puts "-----------------------------------"
-    puts "Are you ready to find your new best friend?\n"
+    puts "\nWELCOME TO BEST FRIEND FINDER!".light_blue
+    puts "--------------------------------"
     start
   end
 
   def start
-    puts "\nWhat kind of friends would you like to meet today? (Please enter the number)\n\n"
+    puts "What kind of friends would you like to find today? (Please enter a number)\n".cyan
 
     PETS.each.with_index(1) do |animal, number|
       puts "#{number}. #{animal}"
@@ -18,11 +17,11 @@ class CLI
     user_input = number_to_index(gets.chomp)
 
     if user_input < 0 || user_input >= PETS.size
-      puts "I'm sorry, that was an incorrect entry. Please try again."
+      puts "\nI'm sorry, that was an incorrect entry. Please try again.\n".red
       start
     end
 
-    puts "\nThank you for your input. One moment as we pull up a list of available #{PETS[user_input].downcase}..."
+    puts "\nThank you. One moment as we pull up a list of available #{PETS[user_input].downcase}...".cyan
 
     @animal_object = define_animal_object(user_input)
     AnimalFactory.new(PETS[user_input], @animal_object)
@@ -32,22 +31,22 @@ class CLI
 
   def display_all_animals
     if @animal_object == Small
-      puts "\n\nAVAILABLE SMALL & FURRY ANIMALS FOR ADOPTION: \n"
+      puts "\n\nAVAILABLE SMALL & FURRY ANIMALS FOR ADOPTION:".light_blue
     else
-      puts "\n\nAVAILABLE #{@animal_object.to_s.upcase} FOR ADOPTION: \n"
+      puts "\n\nAVAILABLE #{@animal_object.to_s.upcase} FOR ADOPTION:".light_blue
     end
-    puts "Name - Breed - Age"
+    puts "Name - Breed - Age".cyan
     puts "-----------------------------"
 
     all_pets = @animal_object.all
     all_pets.each.with_index(1) {|pet, num| puts "#{num}. #{pet.name} - #{pet.breed} - #{pet.age}"}
 
-    puts "\nWhich pet would you like more information on? (Please enter a number)\n"
+    puts "\nWhich pet would you like more information on? (Please enter a number)".cyan
 
     animal_number = number_to_index(gets.chomp)
 
     if animal_number < 0 || animal_number >= all_pets.size
-      puts "I'm sorry, that was an incorrect entry. Please try again."
+      puts "I'm sorry, that was an incorrect entry. Please try again.".red
       display_all_animals
     end
 
@@ -57,16 +56,16 @@ class CLI
   def display_details(num)
       pet = @animal_object.all[num]
       if pet.species == "small-furry"
-        puts "\n#{pet.name.upcase} - Small & Furry"
+        puts "\n#{pet.name.upcase} - Small & Furry".light_blue
       else
-        puts "\n#{pet.name.upcase} - #{pet.species.capitalize}"
+        puts "\n#{pet.name.upcase} - #{pet.species.capitalize}".light_blue
       end
-      puts "------------------------------"
-      puts "Breed: #{pet.breed}"
-      puts "Age: #{pet.age}"
-      puts "Size: #{pet.size}"
-      puts "Color: #{pet.color}"
-      puts "Sex: #{pet.sex}\n\n"
+      puts "------------------------------".cyan
+      puts "Breed:".green + " #{pet.breed}"
+      puts "Age:".green + " #{pet.age}"
+      puts "Size:".green + " #{pet.size}"
+      puts "Color:".green + " #{pet.color}"
+      puts "Sex:".green  + "#{pet.sex}\n\n"
       puts "#{pet.description}\n\n"
       puts "For more info on how to adopt #{pet.name}, visit www.bestfriends.org#{pet.url}"
 
@@ -74,11 +73,10 @@ class CLI
   end
 
   def repeat?
-    puts "\n\nWould you like to search more adoptable pets?\n"
+    puts "\n\nWould you like to search more adoptable pets? (Please enter a number)\n".cyan
     puts "1. Yes, take me back to the list of available #{@animal_object.to_s.downcase}."
     puts "2. Yes, take me back to the main menu."
     puts "3. No, I'm done.\n\n"
-    puts "Please enter a number.\n"
     user_input = gets.chomp
 
     if user_input == "1"
@@ -88,7 +86,7 @@ class CLI
     elsif user_input == "3"
       exit
     else
-      puts "I'm sorry, I didn't understand that answer."
+      puts "I'm sorry, I didn't understand that answer.".red
       repeat?
     end
   end
